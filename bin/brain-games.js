5dee4fa1e25dcb04e37387a9b0764e2660f22cc5
Ainur <lihodeikvint@gmail.com>
2023-04-13 17:50:55 +0500

    add cli.js, update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
+// eslint-disable-next-line import/extensions
+import { getUserName } from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
+const name = getUserName();
+console.log(`Hello, ${name}`);
