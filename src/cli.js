aec1b4048a0484675e67a049f52a519772754cfb
Ainur <lihodeikvint@gmail.com>
2023-04-14 14:04:55 +0500

    rewrite getUserName function

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,12 @@
 import readlineSync from 'readline-sync';
 
-const getUserName = () => readlineSync.question('May I have your name? ');
+const getUserName = () => {
+  const initMessage = 'Welcome to the Brain Games!';
+  const greeting = 'Hello';
+  const questionAskingName = 'May I have your name? ';
+
+  console.log(initMessage);
+  console.log(`${greeting}, ${readlineSync.question(questionAskingName)}`);
+};
 
 export default getUserName;
